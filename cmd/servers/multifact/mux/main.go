4f143c39ad87492b4980651f2bf120917808e889
noellimx <67155581+noellimx@users.noreply.github.com>
2025-01-28 01:10:46 +0800

    + swag

--- cmd/servers/multifact/mux/main.go
+++ cmd/servers/multifact/mux/main.go
@@ -24,21 +24,6 @@ type MuxOpts struct {
 	*AuthMuxOpts
 }
 
-// ShowAccount godoc
-// @Summary      swagger api
-// @Description  swagger docs
-// @Tags         accounts
-// @Produce      json
-// @Param        id   query      string  true  "file name" 	Enums(index.html, doc.json)
-// @Success 200 {string} string "ok, html or json"
-// @Header       200              {string}  Content-Type  "content type"
-// @Failure      404  {object}  int
-// @Failure      500  {object}  int
-// @Router       /swagger [get]
-func swagHandler(w http.ResponseWriter, r *http.Request) {
-	swagger.Handler()(w, r)
-}
-
 // NewMux The closes entry point sans sockets.
 func NewMux(opts *MuxOpts) *http.ServeMux {
 	if opts == nil {
@@ -48,17 +33,15 @@ func NewMux(opts *MuxOpts) *http.ServeMux {
 	}
 
 	mux := http.NewServeMux()
-
 	{
 		helloMux := http.NewServeMux()
-
-		hello := handlers.Hello()
 
 		docs.SwaggerInfo.Description = "This is a ? server."
 		docs.SwaggerInfo.Title = "multifact"
 
-		helloMux.HandleFunc("GET /swagger", swagHandler)
+		helloMux.HandleFunc("GET /swagger", swagger.Handler())
 
+		hello := handlers.Hello()
 		helloMux.HandleFunc("GET /", middlewares.LogMiddleware(func(writer http.ResponseWriter, request *http.Request) {
 			if request.URL.Path != "/" {
 				http.NotFound(writer, request)
@@ -82,32 +65,10 @@ func NewMux(opts *MuxOpts) *http.ServeMux {
 		mux.HandleFunc("POST /register/", middlewares.LogMiddleware(authHandlers.RegisterUsernamePassword()))
 		mux.HandleFunc("POST /token/", middlewares.LogMiddleware(authHandlers.AuthByUsernamePassword()))
 		mux.HandleFunc("POST /otp/", middlewares.LogMiddleware(authHandlers.SubmitOtp()))
-		// ShowAccount godoc
-		// @Summary      Show an account
-		// @Description  get string by ID
-		// @Tags         accounts
-		// @Accept       json
-		// @Produce      json
-		// @Param        id   path      int  true  "Account ID"
-		// @Success      200  {object}  model.Account
-		// @Failure      400  {object}  httputil.HTTPError
-		// @Failure      404  {object}  httputil.HTTPError
-		// @Failure      500  {object}  httputil.HTTPError
-		// @Router       /accounts/{id} [get]
+
 		mux.HandleFunc("PATCH /user/", middlewares.LogMiddleware.Wrap(authMw)(authHandlers.PatchUser()))
 
 		/*
-
-			GET /
-				2025-01-27 07:57:04.939................................REQUEST................................
-				GET / HTTP/1.1
-				Header: Accept: application/json
-
-				2025-01-27 07:57:04.939................................RESPONSE................................
-				HTTP/?.? 200 OK
-				Content-Type: text/plain; charset=utf-8
-				{"data": {"message": "Hello World"}}
-
 			POST /register/
 			POST /token/
 			POST /otp/
--- cmd/servers/multifact/swagger/routes/aaa.go
+++ cmd/servers/multifact/swagger/routes/aaa.go
@@ -1,16 +1,9 @@
-package mux
+package routes
 
-// ShowAccount godoc
-// @Summary      swagger api
-// @Description  swagger docs
-// @Tags         accounts
-// @Produce      json
-// @Param        id   query      string  true  "file name" 	Enums(index.html, doc.json)
-// @Success 200 {string} string "ok, html or json"
-// @Header       200              {string}  Content-Type  "content type"
-// @Failure      404  {object}  int
-// @Failure      500  {object}  int
-// @Router       /swagger [get]
+import (
+	"net/http"
+)
+
 func SwagHandler(w http.ResponseWriter, r *http.Request) {
-	swagger.Handler()(w, r)
+
 }
