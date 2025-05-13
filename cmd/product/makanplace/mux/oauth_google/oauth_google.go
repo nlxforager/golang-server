ca094e5a86730369e9aacb6da9cb1070297ce430
noellimx <67155581+noellimx@users.noreply.github.com>
2025-05-13 11:44:45 +0800

    return on http status set

--- cmd/product/makanplace/mux/oauth_google/oauth_google.go
+++ cmd/product/makanplace/mux/oauth_google/oauth_google.go
@@ -12,17 +12,16 @@ import (
 	"google.golang.org/api/oauth2/v2"
 )
 
-func Register(mux *http.ServeMux, makanTokenCookieKey string, goAuthService *goauthservice.Service, mkService *mkusersessionservice.Service) {
+func Register(mux *http.ServeMux, makanTokenCookieKey string, gOAuthService *goauthservice.Service, mkService *mkusersessionservice.Service) {
 	mux.HandleFunc("/auth/google/login", func(w http.ResponseWriter, r *http.Request) {
-		redirUrl := goAuthService.AuthCodeURL()
+		redirUrl := gOAuthService.AuthCodeURL()
 		http.Redirect(w, r, redirUrl, http.StatusTemporaryRedirect)
 	})
 
 	// client will provide authCode and state.
 	// if we are able to exchange a valid access token and use it to obtain user info, we will associate the google credential to a makanplace user.
 
-	log.Println(goAuthService.AuthCodeSuccessCallbackPath())
-	mux.HandleFunc(goAuthService.AuthCodeSuccessCallbackPath(), func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(gOAuthService.AuthCodeSuccessCallbackPath(), func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if r := recover(); r != nil {
 				log.Printf("panic: %v", r)
@@ -32,7 +31,7 @@ func Register(mux *http.ServeMux, makanTokenCookieKey string, goAuthService *goa
 		authCode := r.URL.Query().Get("code")
 		state := r.URL.Query().Get("state")
 
-		userInfo, err := goAuthService.UserInfo(state, authCode)
+		userInfo, err := gOAuthService.UserInfo(state, authCode)
 		if err != nil {
 			w.WriteHeader(http.StatusForbidden)
 		}
@@ -45,6 +44,7 @@ func Register(mux *http.ServeMux, makanTokenCookieKey string, goAuthService *goa
 		sessionId, err := mkService.CreateUserSession([]*oauth2.Userinfo{userInfo})
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Set-Cookie", makanTokenCookieKey+"="+sessionId+"; path=/; HttpOnly")
